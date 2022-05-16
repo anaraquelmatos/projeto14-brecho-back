f33806ee2bae278fe44dc7830a8b0ca7c79128c7
Ana Raquel Matos <anaraquelpmatos@gmail.com>
2022-05-16 14:37:34 -0300

    update: admin

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,17 +1,12 @@
 import db from "../db.js";
+import joi from "joi";
 
 export async function adminController(req, res) {
 
-
-    const { category, type, itemName, image, storePrice, price, discount, quantity, description, collection, brand, dimensions } = req.body;
-
-    const array = await db.collection("products").find().toArray();
+    const { category, type, itemName, image, storePrice, price, discount, size, color, description,
+        collection, brand, dimensions } = req.body;
 
     let id = 0;
-
-    for (let i = 0; i < array.length; i++) {
-        id = i + 1;
-    }
 
     const register = {
         id: id,
@@ -22,12 +17,24 @@ export async function adminController(req, res) {
         storePrice,
         price,
         discount,
-        quantity,
+        quantity: [
+            {
+                size,
+                color
+            }
+        ],
         description,
         collection,
         brand,
         dimensions
     }
+
+    const array = await db.collection("products").find().toArray();
+
+    for (let i = 0; i < array.length; i++) {
+        id = i + 1;
+    }
+
     try {
 
         const user = await db.collection("users").findOne({ email: 'admin@gmail.com' });
@@ -37,6 +44,8 @@ export async function adminController(req, res) {
         })
         res.send(register);
         console.log(register)
+        console(quantity.color)
+        console(quantity.size)
 
     } catch (e) {
         console.log(e);
