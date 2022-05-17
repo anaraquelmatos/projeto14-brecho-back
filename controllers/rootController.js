afbfba267aeb22875f497fb5ea71618c93de4361
Ana Raquel Matos <anaraquelpmatos@gmail.com>
2022-05-17 19:16:10 -0300

    add: delete token

--- controllers/rootController.js
+++ controllers/rootController.js
@@ -13,3 +13,22 @@ export async function rootController(req, res) {
         res.sendStatus(500);
     }
 }
+
+export async function deleteAccessRootController(req, res) {
+
+    const { id } = req.params;
+
+    try {
+        const session = await db.collection("sessions").findOne({ token: id });
+        if (session) {
+            await db.collection("sessions").deleteOne({ token: id });
+            res.sendStatus(201);
+
+        } else {
+            res.sendStatus(404);
+        }
+
+    } catch {
+        res.sendStatus(500);
+    }
+}
